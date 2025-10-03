8a46798c909fe5d0f6a6bc0ce076262b9c74e38b
Kenji Hashimoto <hashimoto.kenji@kagawa-u.ac.jp>
2025-10-03 12:45:07 +0900

    add sponsorship.html

--- src/_data/nav.js
+++ src/_data/nav.js
@@ -7,7 +7,7 @@ module.exports = [
   { label: "募集要項", url: "calling.html", enabled: true },
   { label: "投稿・発表申込要項", url: "submission.html", enabled: false },
   { label: "現地案内", url: "access.html", enabled: false },
-  { label: "スポンサー募集", url: "sponsorship.html", enabled: false },
+  { label: "スポンサー募集", url: "sponsorship.html", enabled: true },
   { label: "アンチハラスメントポリシー", url: "anti-harrasment-policy.html", enabled: true },
   { label: "運営組織", url: "organization.html", enabled: true },
   { label: "お問い合わせ先", url: "contact.html", enabled: true },
