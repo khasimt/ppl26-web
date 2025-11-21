e6527eafa2f78a999c0df9390db77bc33dbd49d2
Kenji Hashimoto <hashimoto.kenji@kagawa-u.ac.jp>
2025-11-21 19:38:53 +0900

    add submission.html

--- src/_data/nav.js
+++ src/_data/nav.js
@@ -5,7 +5,7 @@ module.exports = [
   { label: "プログラム", url: "program.html", enabled: false },
   { label: "参加登録要領", url: "registration.html", enabled: false },
   { label: "募集要項", url: "calling.html", enabled: true },
-  { label: "投稿・発表申込要項", url: "submission.html", enabled: false },
+  { label: "投稿・発表申込要項", url: "submission.html", enabled: true },
   { label: "現地案内", url: "access.html", enabled: false },
   { label: "スポンサー募集", url: "sponsorship.html", enabled: true },
   { label: "アンチハラスメントポリシー", url: "anti-harrasment-policy.html", enabled: true },
