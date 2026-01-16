33a884dbc449672c818212f6ab1ecaa122dea444
Kenji Hashimoto <hashimoto.kenji@kagawa-u.ac.jp>
2026-01-16 16:27:03 +0900

    add registration.html

--- src/_data/nav.js
+++ src/_data/nav.js
@@ -3,7 +3,7 @@ module.exports = [
   { label: "招待講演", url: "invited.html", enabled: true }, 
   { label: "採択リスト", url: "accepted.html", enabled: false },
   { label: "プログラム", url: "program.html", enabled: false },
-  { label: "参加登録要領", url: "registration.html", enabled: false },
+  { label: "参加登録要領", url: "registration.html", enabled: true },
   { label: "募集要項", url: "calling.html", enabled: true },
   { label: "投稿・発表申込要項", url: "submission.html", enabled: true },
   { label: "現地案内", url: "access.html", enabled: true },
